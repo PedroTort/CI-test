c6622b046e59a55e1daf6fa0638c685f20db38a1
PedroTort <pedper@alunos.utfpr.edu.br>
2024-11-08 10:45:04 -0300

    falha

--- playwright/tests/example.test.ts
+++ playwright/tests/example.test.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('should load the React app and display text', async ({ page }) => {
+  test.setTimeout(5000)
   // Navigate to the React app (ensure it's running on localhost:3000)
   await page.goto('https://www.google.com.br')
-  await page.pause()
+  // await page.pause()
   // Wait for the page to load and check for the header text (replace as needed)
-  page.getByLabel('Pesquisar', { exact: true })
+  await page.getByLabel('falha', { exact: true }).click()
 });
