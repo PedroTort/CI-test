5b86ae5f2772b2f6f8b609242ec9fa1bb6debf4c
PedroTort <pedper@alunos.utfpr.edu.br>
2024-11-08 10:47:08 -0300

    agora vai

--- playwright/tests/example.test.ts
+++ playwright/tests/example.test.ts
@@ -1,10 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('should load the React app and display text', async ({ page }) => {
-  test.setTimeout(5000)
   // Navigate to the React app (ensure it's running on localhost:3000)
-  await page.goto('https://www.google.com.br')
-  // await page.pause()
+  await page.goto('http://localhost:3000')
   // Wait for the page to load and check for the header text (replace as needed)
-  await page.getByLabel('falha', { exact: true }).click()
+  page.getByRole('link', { name: 'Learn React' })
 });
