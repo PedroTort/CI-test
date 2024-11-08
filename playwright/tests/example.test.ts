245b6857a23a656ad392fecfdde335ac573c8ac1
PedroTort <pedper@alunos.utfpr.edu.br>
2024-11-08 13:36:08 -0300

    local test

--- playwright/tests/example.test.ts
+++ playwright/tests/example.test.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('should load the React app and display text', async ({ page }) => {
-  // await page.goto('http://localhost:3000')
-  
-  await page.goto('www.google.com.br')
-  // page.getByRole('link', { name: 'Learn Reactson', exact: true })
+  await page.goto('http://localhost:3000')
+
+  // await page.goto('www.google.com.br')
+  page.getByRole('link', { name: 'Learn Reactson', exact: true })
 });
