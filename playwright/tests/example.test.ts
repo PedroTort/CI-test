ac09b3ed0e62c011179a56e28ad9def0c084e259
PedroTort <pedper@alunos.utfpr.edu.br>
2024-11-08 10:27:58 -0300

    google test

--- playwright/tests/example.test.ts
+++ playwright/tests/example.test.ts
@@ -2,8 +2,8 @@ import { test, expect } from '@playwright/test';
 
 test('should load the React app and display text', async ({ page }) => {
   // Navigate to the React app (ensure it's running on localhost:3000)
-  await page.goto('http://localhost:3000')
+  await page.goto('https://www.google.com.br')
   await page.pause()
   // Wait for the page to load and check for the header text (replace as needed)
-  page.getByRole('link', { name: 'Learn React' })
+  page.getByLabel('Pesquisar', { exact: true })
 });
