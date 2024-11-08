0d2ba804e622196a1760347a98526e96c19a5b7a
PedroTort <pedper@alunos.utfpr.edu.br>
2024-11-08 13:24:04 -0300

    path change for report

--- playwright/tests/example.test.ts
+++ playwright/tests/example.test.ts
@@ -3,7 +3,8 @@ import { test, expect } from '@playwright/test';
 test('should load the React app and display text', async ({ page }) => {
   // Navigate to the React app (ensure it's running on localhost:3000)
   // await page.goto('http://localhost:3000')
-  await page.goto('http://localhost:3000')
+  await page.goto('www.google.com.br')
+  // await page.goto('http://localhost:3000')
   // Wait for the page to load and check for the header text (replace as needed)
   page.getByRole('link', { name: 'Learn Reactson', exact: true })
 });
